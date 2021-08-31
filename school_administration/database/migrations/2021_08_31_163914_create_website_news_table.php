17f7ddd98e56eaddecdc28c506361807da68b9cc
nero-ax <89801792+nero-ax@users.noreply.github.com>
2021-08-31 18:48:40 +0200

    create website_news migration

---
+++ school_administration/database/migrations/2021_08_31_163914_create_website_news_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWebsiteNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('website_news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('short_story');
+            $table->string('full_story');
+            $table->string('author');
+            $table->string('image');
+            $table->enum('hidden', [0, 1]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('website_news');
+    }
+}
