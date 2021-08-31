406872daac6075166c9bd5f6f8f6d4ad2e4f1999
nero-ax <89801792+nero-ax@users.noreply.github.com>
2021-08-31 14:17:48 +0200

    update profile controller, implement changing password

--- school_administration/app/Http/Controllers/ProfileController.php
+++ school_administration/app/Http/Controllers/ProfileController.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = Auth::User();
+        return view('profile', [
+            'user' => $user,
+        ]);
+    }
+
+    public function updatePassword(Request $request) {
+        $user = Auth::User();
+        $validated = $request->validate([
+            'password_old' => ['required', 'string', 'min:8'],
+            'password_new' => ['required', 'string', 'min:8', 'same:password_new_confirm'],
+            'password_new_confirm' => ['required', 'string', 'min:8'],
+        ]);
+
+        if(Hash::make($validated['password_old']) == $user->password) {
+            $user->password = Hash::make($validated['password_new']);
+            $user->save();
+            return redirect('settings')->with('status', 'Password updated successfully.');
+        } else {
+            return redirect('settings')->with('status', 'Authentication error.');
+        }
+    }
 }
